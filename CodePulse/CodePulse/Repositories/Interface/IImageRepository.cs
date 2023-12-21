8dbe4b471731001db487e811ee8c4f2b1177fe0d
hiccup <hiccup1803@gmail.com>
2023-12-21 02:06:41 -0500

    12_21

---
+++ CodePulse/CodePulse/Repositories/Interface/IImageRepository.cs
@@ -0,0 +1,11 @@
+﻿using CodePulse.API.Models.Domains;
+
+namespace CodePulse.API.Repositories.Interface
+{
+    public interface IImageRepository
+    {
+        Task<BlogImage> Upload(IFormFile file, BlogImage blogImage);
+
+        Task<IEnumerable<BlogImage>> GetAll();
+    }
+}
